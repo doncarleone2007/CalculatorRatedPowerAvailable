31919f83efd6986aad96377c54a1eeba583d1ce1
Eldor Temirov <doncarleone2007@gmail.com>
2021-03-18 03:55:40 +0500

    fsd

--- CalculatorRatedPowerAvailable/ResultForm.cs
+++ CalculatorRatedPowerAvailable/ResultForm.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Text;
@@ -19,7 +20,122 @@ namespace CalculatorRatedPowerAvailable
         {
             InitializeComponent();
             btnDownloadCalculateWord.Click += BtnDownloadCalculateWord_Click;
+            btnDownloadDocumentationWord.Click += BtnDownloadDocumentationWord_Click;
+
+            btnDownloadCalculatePDF.Click += BtnDownloadCalculatePDF_Click;
+            btnDownloadDocumentationPDF.Click += BtnDownloadDocumentationPDF_Click;
             this.Load += ResultForm_Load;
+        }
+
+        private void BtnDownloadDocumentationPDF_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.InitialDirectory = @"C:\";
+            saveFileDialog1.Title = "Save PDF Files";
+            //saveFileDialog1.CreatePrompt = true;
+            saveFileDialog1.OverwritePrompt = true;
+            //saveFileDialog1.CheckFileExists = true;
+            //saveFileDialog1.CheckPathExists = true;
+            saveFileDialog1.DefaultExt = "pdf";
+            saveFileDialog1.Filter = "PDF files (*.pdf)|*.pdf";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Document doc = new Document();
+                Section section = doc.AddSection();
+                Paragraph Para = section.AddParagraph();
+                Para.AppendRTF(richTextBoxResultDocument.Rtf);
+
+                doc.SaveToFile(saveFileDialog1.FileName, FileFormat.PDF);
+                //Convert to PDF
+                //document.SaveToFile(pdfPath, FileFormat.PDF);
+                MessageBox.Show("Создан результат расчёта в Word файле", "Выполнен процесс", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                doc.Close();
+
+                try
+                {
+                    Process.Start(saveFileDialog1.FileName);
+                }
+                catch
+                {
+                }
+            }
+        }
+
+        private void BtnDownloadCalculatePDF_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.InitialDirectory = @"C:\";
+            saveFileDialog1.Title = "Save PDF Files";
+            //saveFileDialog1.CreatePrompt = true;
+            saveFileDialog1.OverwritePrompt = true;
+            //saveFileDialog1.CheckFileExists = true;
+            //saveFileDialog1.CheckPathExists = true;
+            saveFileDialog1.DefaultExt = "pdf";
+            saveFileDialog1.Filter = "PDF files (*.pdf)|*.pdf";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Document doc = new Document();
+                Section section = doc.AddSection();
+                Paragraph Para = section.AddParagraph();
+                Para.AppendRTF(richTextBoxResultCalculate.Rtf);
+
+                doc.SaveToFile(saveFileDialog1.FileName, FileFormat.PDF);
+                //Convert to PDF
+                //document.SaveToFile(pdfPath, FileFormat.PDF);
+                MessageBox.Show("Создан результат расчёта в PDF файле", "Выполнен процесс", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                doc.Close();
+
+                try
+                {
+                    Process.Start(saveFileDialog1.FileName);
+                }
+                catch
+                {
+                }
+            }
+        }
+
+        private void BtnDownloadDocumentationWord_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.InitialDirectory = @"C:\";
+            saveFileDialog1.Title = "Save Word Files";
+            //saveFileDialog1.CreatePrompt = true;
+            saveFileDialog1.OverwritePrompt = true;
+            //saveFileDialog1.CheckFileExists = true;
+            //saveFileDialog1.CheckPathExists = true;
+            saveFileDialog1.DefaultExt = "docx";
+            saveFileDialog1.Filter = "Word files (*.docx)|*.docx";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Document doc = new Document();
+                Section section = doc.AddSection();
+                Paragraph Para = section.AddParagraph();
+                Para.AppendRTF(richTextBoxResultDocument.Rtf);
+
+                doc.SaveToFile(saveFileDialog1.FileName, FileFormat.Docx);
+                //Convert to PDF
+                //document.SaveToFile(pdfPath, FileFormat.PDF);
+                MessageBox.Show("Создан результат расчёта в Word файле", "Выполнен процесс", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                doc.Close();
+
+                try
+                {
+                    Process.Start(saveFileDialog1.FileName);
+                }
+                catch
+                {
+                }
+            }
         }
 
         private void BtnDownloadCalculateWord_Click(object sender, EventArgs e)
@@ -48,12 +164,24 @@ namespace CalculatorRatedPowerAvailable
                 //document.SaveToFile(pdfPath, FileFormat.PDF);
                 MessageBox.Show("Создан результат расчёта в Word файле", "Выполнен процесс", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 doc.Close();
+
+                try
+                {
+                    Process.Start(saveFileDialog1.FileName);
+                }
+                catch
+                {
+                }
             }
         }
 
-        Dictionary<string, string> GetReplaceResultCalculateDictionary()
+        Dictionary<string, string> GetReplaceResultDictionary()
         {
             Dictionary<string, string> replaceDict = new Dictionary<string, string>();
+            
+            replaceDict.Add("#grsname#", Result.GrsName.ToString());
+            replaceDict.Add("#subgrsname#", Result.SubGrsName.ToString());
+            replaceDict.Add("#msm#", Result.Msantimeter.ToString());
             replaceDict.Add("#msm#", Result.Msantimeter.ToString());
             replaceDict.Add("#bigr#", Result.R.ToString());
             replaceDict.Add("#smallk#", Result.K.ToString());
@@ -104,7 +232,7 @@ namespace CalculatorRatedPowerAvailable
             var document = new Document();
             document.LoadRtf(Path.Combine(Environment.CurrentDirectory, "calculateTemplate.rtf"));
             //get strings to replace
-            Dictionary<string, string> dictReplace = GetReplaceResultCalculateDictionary();
+            Dictionary<string, string> dictReplace = GetReplaceResultDictionary();
             //Replace text
             foreach (KeyValuePair<string, string> kvp in dictReplace)
             {
@@ -115,6 +243,24 @@ namespace CalculatorRatedPowerAvailable
             document.Close();
 
             richTextBoxResultCalculate.LoadFile(Path.Combine(Environment.CurrentDirectory, $"resultCalculate.rtf"));
+        }
+
+        private void SetRtfToRichtextBoxResultDocument()
+        {
+            var document = new Document();
+            document.LoadRtf(Path.Combine(Environment.CurrentDirectory, "documentTemplate.rtf"));
+            //get strings to replace
+            Dictionary<string, string> dictReplace = GetReplaceResultDictionary();
+            //Replace text
+            foreach (KeyValuePair<string, string> kvp in dictReplace)
+            {
+                document.Replace(kvp.Key, kvp.Value, true, true);
+            }
+
+            document.SaveToFile(Path.Combine(Environment.CurrentDirectory, $"resultDocument.rtf"), FileFormat.Rtf);
+            document.Close();
+
+            richTextBoxResultDocument.LoadFile(Path.Combine(Environment.CurrentDirectory, $"resultDocument.rtf"));
         }
     }
 }
